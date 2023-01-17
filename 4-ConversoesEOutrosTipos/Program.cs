0321f41a7093fac64b89c383b6ec80631ae73601
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-17 17:57:38 -0300

    Conversão de Tipos

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        double salario;
+        salario = 3000.15;
+
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+
+        Console.WriteLine(salarioInteiro);
+        Console.ReadLine();
+
+    }
+}
